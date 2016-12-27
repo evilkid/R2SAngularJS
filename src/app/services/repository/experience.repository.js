063f489803645af3a84280c52b9adaafa24a6b11
Ouerghi Yassine <ouerghi.yassine@hotmail.com>
2016-12-27 16:32:11 +0100

    Certification/Experience Resource

---
+++ src/app/services/repository/experience.repository.js
@@ -0,0 +1,18 @@
+(function (angular) {
+    "use strict";
+    angular.module('app.repository')
+        .factory('Experience', ExperienceFactory);
+
+    /** @ngInject */
+    function ExperienceFactory($resource, API) {
+        var params = {id: "@id"};
+        var customMethods = {
+            'update': {
+                method: "PUT"
+            }
+        };
+        var Experience = $resource(API + "/experience/:id", params, customMethods);
+        return Experience;
+    }
+
+})(angular);
